5702f63669fe5bd822609ca8e88b2aa822a7a5d5
Isti Anahtur Rizqiyah <148040476+IstiAnahturRizqiyah@users.noreply.github.com>
2023-10-26 22:30:34 +0700

    Delete tugas2.js

--- tugas2.js
+++
@@ -1,8 +0,0 @@
-//membuat variabel
-var a = "isty";
-var b = "jakarta selatan"
-var c = "Bacth 7"
-//menampilkan diterminal
-console.log ("nama saya ", a)
-console.log ("alamat saya ", b)
-console.log ("latihan ", c)
